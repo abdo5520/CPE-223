6946464e2282735f2fda0be22f73c647cd1548be
Abdallah Abdallah <abdo.aast@gmail.com>
2025-02-10 17:42:54 -0500

    Add files via upload

--- Chapter2_12.c
+++ Chapter2_12.c
@@ -2,14 +2,34 @@
 #include <stdio.h>
 
 int main(void) {
+
+
    char arrowBody;
    char arrowHead;
 
-   char message[] = "\"hello world\"";
+   arrowBody = '-';
+   arrowHead = '>';
+
+   printf("%c%c%c%c\n", arrowBody, arrowBody, arrowBody, arrowHead);
+
+   arrowBody = 'o';
+
+   printf("%c%c%c%c\n", arrowBody, arrowBody, arrowBody, arrowHead);
+
+
+   char message[] = "hello world";
 
 
 
     printf("%s\n", message);
+
+
+
+
+      char message2[] = "\"hello world\"";
+       printf("%s\n", message2);
+
+
 
 
    return 0;
