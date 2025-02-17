f8ff677bf13ae36e8868a1f4b50e7f612a6ae169
Abdallah Abdallah <abdo.aast@gmail.com>
2025-02-17 16:19:36 -0500

    Add files via upload

---
+++ ch2_overflow.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+ //  int num;
+
+ long long num;
+
+
+   num = 100;
+
+   num = num * 100;
+   printf("num: %d\n", num);
+
+   num = num * 100;
+   printf("num: %d\n", num);
+
+   num = num * 100;
+   printf("num: %d\n", num);
+
+   num = num * 100;
+   printf("num: %lld\n", num);
+
+   num = num * 100;
+   printf("num: %lld\n", num);
+
+   num = num * 100;
+   printf("num: %lld\n", num);
+
+   return 0;
+}
